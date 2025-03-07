8cb4c0addf337a995189a5a67f3d36848e698ffd
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-03-07 13:03:34 +0100

    refactor navbar into its own file

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -1,46 +1,11 @@
 // import { useState } from 'react'
 import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 export const App = () => {
   // const [count, setCount] = useState(0)
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
-      <div className="container-fluid">
-        <span className="navbar-brand">Good Read</span>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Search Books
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item m-1">
-              <a type="button" className="btn btn-outline-light" href="#">
-                Sign in
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
   );
 }
